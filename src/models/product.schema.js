9bd88cd3aeb8b006cd1e6d7dc2aa09a5952d34b3
chirag <chiragsinghal04@gmail.com>
2024-06-02 08:20:45 +0530

    -expenses

--- src/models/product.schema.js
+++ src/models/product.schema.js
@@ -1,31 +1,89 @@
 import mongoose from "mongoose";
 
 export const productSchema = new mongoose.Schema({
-    itemImage:{
-        data:Buffer,
-        contentType:String
+    itemImage: {
+        data: Buffer,
+        contentType: String
     },
-    itemName: String,
-    category:String,
-    fabricator:String,
-    clothRate:Number,
-    clothName:String,
-    averagePiece:Number,
-    purchaseRate:Number,
-    totalExpense:Number,
-    quantityS:Number,
-    quantityM:Number,
-    quantityL:Number,
-    
-    quantityXL:Number,
-    quantityXXL:Number,
-    
-    fabrication:Number,
-    sizeWiseRateS:Number,
-    sizeWiseRateMLXL:Number,
-    sizeWiseRateXXL:Number,
-    margin:Number,
-    discount:Number,
-    packingCharge:Number
-
+    itemName: {
+        type: String,
+        default: ""
+    },
+    category: {
+        type: String,
+        default: ""
+    },
+    fabricator: {
+        type: String,
+        default: ""
+    },
+    clothRate: {
+        type: Number,
+        default: 0
+    },
+    clothName: {
+        type: String,
+        default: ""
+    },
+    averagePiece: {
+        type: Number,
+        default: 0
+    },
+    purchaseRate: {
+        type: Number,
+        default: 0
+    },
+    totalExpense: {
+        type: Number,
+        default: 0
+    },
+    quantityS: {
+        type: Number,
+        default: 0
+    },
+    quantityM: {
+        type: Number,
+        default: 0
+    },
+    quantityL: {
+        type: Number,
+        default: 0
+    },
+    quantityXL: {
+        type: Number,
+        default: 0
+    },
+    quantityXXL: {
+        type: Number,
+        default: 0
+    },
+    fabrication: {
+        type: Number,
+        default: 0
+    },
+    sizeWiseRateS: {
+        type: Number,
+        default: 0
+    },
+    sizeWiseRateMLXL: {
+        type: Number,
+        default: 0
+    },
+    sizeWiseRateXXL: {
+        type: Number,
+        default: 0
+    },
+    margin: {
+        type: Number,
+        default: 0
+    },
+    discount: {
+        type: Number,
+        default: 0
+    },
+    packingCharge: {
+        type: Number,
+        default: 0
+    }
 });
+
